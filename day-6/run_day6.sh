ef0ce493c8b6d9a0da8baa266f785ad2144b8ee7
NikaMalinovskaia <120171733+NikaMalinovskaia@users.noreply.github.com>
2026-09-12 17:31:36 +0600

    un_day6.sh

--- day-6/run_day6.sh
+++ day-6/run_day6.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Экспортируем ключ для доступа через шлюз
+# Экспортируем ключ
 export LITELLM_API_KEY="sk-L2Fx4Xsvy_OA6gcp3gG2pA"
 
 echo "Запуск агента (День 6)..."
 python3 main_day6.py
 
-echo "Готово!"
+# Проверяем код возврата Python ($?)
+if [ $? -eq 0 ]; then
+    echo "День 6 успешно выполнен!"
+else
+    echo "День 6 завершился с ошибкой!"
+    exit 1
+fi
